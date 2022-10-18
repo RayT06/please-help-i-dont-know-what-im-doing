34f326912f2a5747e6e266671a8c8efad439b88b
RayT06 <116083789+RayT06@users.noreply.github.com>
2022-10-18 14:46:47 +0000

    Rounding, Truncate

--- main.cs
+++ main.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program {
   public static void Main (string[] args) {
@@ -14,5 +16,41 @@ class Program {
 
     //output the random month name
     Console.WriteLine("Month is {0}", months[month-1]);
+    RangeTest();
+    RoundTest();
+    TruncateTest();
   }
+
+public static void RangeTest() {
+  IEnumerable<int> squares = Enumerable.Range(1, 10).Select(x => x* x);
+
+foreach (int num in squares) {
+  Console.WriteLine(num);
 }
+}
+
+public static void RoundTest() {
+  float originalNumber1 = 1.23f;
+  float originalNumber2 = 3.15f;
+  float originalNumber3 = 1.9f;
+
+  float roundedNumber1 = (float)Math.Ceiling(originalNumber1);
+  float roundedNumber2 = (float)Math.Round(originalNumber2, 1);
+  float roundedNumber3 = (float)Math.Floor(originalNumber3);
+  
+  Console.WriteLine("roundedNumber1 is " + roundedNumber1);
+  Console.WriteLine("roundedNumber2 is " + roundedNumber2);
+  Console.WriteLine("roundedNumber3 is " + roundedNumber3);
+  }
+
+ public static void TruncateTest()
+ {   
+   decimal decimalNumber;
+
+   decimalNumber = 32.7865m ;
+   Console.WriteLine(Math.Truncate(decimalNumber));
+   decimalNumber = -32.9012m;
+   Console.WriteLine(Math.Truncate(decimalNumber));
+  } 
+}
+
